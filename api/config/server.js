b0e31bbc1a1b03089d8a43f7111be448b5ebdf2e
Felipe Barcelos <felipebarcelospro@Felipes-iMac-Pro.local>
2021-02-21 12:34:40 -0300

    first commit

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '86c5bb4a901b81b8fac442e627345e50'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '93d5dc7f-6c5f-4a69-8120-1631140a38df'
+};
